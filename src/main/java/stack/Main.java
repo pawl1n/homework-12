f307c9d66fa343cc9c843e6cbdf118c061bcafd9
pawl1n <pinchuk.andryusha310212@gmail.com>
2022-11-18 14:51:20 +0200

    feat: stack

--- src/main/java/stack/Main.java
+++ src/main/java/stack/Main.java
@@ -1,7 +1,30 @@
 package stack;
 
+import stack.exceptoins.FullStackException;
+
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
+        MyStack<String> stack = new MyStack<>();
 
+        Optional<String> item = stack.popOptional();
+        String str = item.orElse("Empty string");
+        System.out.println(str);
+
+        try {
+            stack.push("first string");
+            stack.push("second string");
+            stack.push("third string");
+            stack.push("fourth string");
+            stack.push("fifth string");
+            stack.push("sixth string");
+            stack.push("seventh string");
+
+        } catch (FullStackException e) {
+            System.out.println("Stack is full");
+        }
+
+        System.out.println(stack);
     }
 }
