b845182567ce301a03df67f9a4fa5ff2dc572bdb
pawl1n <pinchuk.andryusha310212@gmail.com>
2022-11-18 14:50:55 +0200

    feat: empty and full stack exceptions

---
+++ src/main/java/stack/exceptoins/FullStackException.java
@@ -0,0 +1,12 @@
+package stack.exceptoins;
+
+public class FullStackException extends Exception {
+    public FullStackException() {
+        super();
+    }
+
+    public FullStackException(String message) {
+        super(message);
+    }
+
+}
